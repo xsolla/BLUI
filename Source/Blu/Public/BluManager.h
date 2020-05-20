d22cf8af16cb816692819a9a73bebde188c5501d
getnamo <getnamo@gmail.com>
2020-05-20 14:52:23 -0700

    move cef includes to a single file

--- Source/Blu/Public/BluManager.h
+++ Source/Blu/Public/BluManager.h
@@ -1,23 +1,6 @@
 #pragma once
 
-#if PLATFORM_WINDOWS
-#include "Windows/WindowsHWrapper.h"
-#include "Windows/AllowWindowsPlatformTypes.h"
-#include "Windows/AllowWindowsPlatformAtomics.h"
-#endif
-//#pragma push_macro("OVERRIDE")
-//#undef OVERRIDE // cef headers provide their own OVERRIDE macro
-THIRD_PARTY_INCLUDES_START
-#include "include/cef_app.h"
-#include "include/cef_browser.h"
-
-THIRD_PARTY_INCLUDES_END
-//#pragma pop_macro("OVERRIDE")
-#if PLATFORM_WINDOWS
-#include "Windows/HideWindowsPlatformAtomics.h"
-#include "Windows/HideWindowsPlatformTypes.h"
-#endif
-
+#include "CEFInclude.h"
 
 class BLU_API BluManager : public CefApp
 {
--- Source/Blu/Public/RenderHandler.h
+++ Source/Blu/Public/RenderHandler.h
@@ -1,27 +1,10 @@
 #pragma once
 
+#include "CEFInclude.h"
 #include "BluTypes.h"
 //#include "BluEye.h"
 class UBluEye;
 
-
-
-#if PLATFORM_WINDOWS
-#include "Windows/WindowsHWrapper.h"
-#include "Windows/AllowWindowsPlatformTypes.h"
-#include "Windows/AllowWindowsPlatformAtomics.h"
-#endif
-//#pragma push_macro("OVERRIDE")
-//#undef OVERRIDE // cef headers provide their own OVERRIDE macro
-THIRD_PARTY_INCLUDES_START
-#include "include/cef_client.h"
-#include "include/cef_app.h"
-THIRD_PARTY_INCLUDES_END
-//#pragma pop_macro("OVERRIDE")
-#if PLATFORM_WINDOWS
-#include "Windows/HideWindowsPlatformAtomics.h"
-#include "Windows/HideWindowsPlatformTypes.h"
-#endif
 
 class RenderHandler : public CefRenderHandler
 {
